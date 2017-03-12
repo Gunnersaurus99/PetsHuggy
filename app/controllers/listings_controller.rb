7e59d171ba057fb86064290c92565e3bb627f745
Gooner <miyazoekodai@miyatenkoudainoMacBook-Pro.local>
2017-03-12 17:41:09 +0900

    add gems for Heroku

---
+++ app/controllers/listings_controller.rb
@@ -0,0 +1,97 @@
+class ListingsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_listing, only: [:edit,:basics,:update,:description,:address,:price,:photos,:calender,:bankaccount,:publish]
+
+  def index
+  end
+
+  def show
+  end
+
+  def create
+    @listing = current_user.listings.build(listing_params)
+    if @listing.save
+      redirect_to manage_listing_basics_path(@listing), notice: "リスティングを作成・保存しました"
+    else
+      redirect_to new_listing_path, notice: "リスティングを作成・保存できませんでした"
+    end
+  end
+
+  def edit
+  end
+
+  def basics
+  end
+
+  def new
+    @listing = current_user.listings.build
+  end
+
+  def update
+    if @listing.update(listing_params)
+      redirect_to :back, notice: "更新できました"
+    end
+  end
+
+  def description
+  end
+
+
+  def address
+  end
+
+
+  def price
+  end
+
+
+  def photos
+    @photo = Photo.new
+  end
+
+
+  def calender
+  end
+
+
+  def bankaccount
+  end
+
+
+  def publish
+  end
+
+
+  private
+   def listing_params
+    params.require(:listing).permit(:pet_type,:home_type,:breeding_years,:pet_size,:price_per_night,:address)
+  end
+
+   def set_listing
+    @listing= Listing.find(params[:id])
+   end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+end
